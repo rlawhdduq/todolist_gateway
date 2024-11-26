61718d2bcd03cc1ccd901ac18776534f3de54af2
kjy <rlawhdduq95@gmail.com>
2024-11-26 15:19:53 +0900

    최초 업로드

---
+++ src/test/java/todolist/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
