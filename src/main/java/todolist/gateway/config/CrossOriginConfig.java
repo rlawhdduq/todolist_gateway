dc0ac2e787acc614494703a659ddc3698f2d3f8c
kjy <rlawhdduq95@gmail.com>
2024-12-12 19:42:51 +0900

    확인용...

--- src/main/java/todolist/gateway/config/CrossOriginConfig.java
+++ src/main/java/todolist/gateway/config/CrossOriginConfig.java
@@ -1,19 +1,24 @@
 package todolist.gateway.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class CrossOriginConfig implements WebMvcConfigurer{
 
     @Value("${allow.host.url}")
     private String allowHostUrl;
-
+    private static final Logger log = LoggerFactory.getLogger(CrossOriginConfig.class);
     @Override
     public void addCorsMappings(CorsRegistry registry)
     {
+        log.info("cors origin 들어왔어요~");
+        log.info(registry.toString());
         registry.addMapping("/api/v1/service")
                 .allowedOriginPatterns("http://front", "http://localhost")
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
