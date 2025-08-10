9976531a7750145e3097e453afba0d9cfd53f411
kjy <rlawhdduq95@gmail.com>
2025-08-11 00:33:03 +0900

    구독서비스 변경(redis > api)

--- src/main/java/todolist/gateway/api/Gateway.java
+++ src/main/java/todolist/gateway/api/Gateway.java
@@ -2,15 +2,23 @@ package todolist.gateway.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import todolist.gateway.dto.BoardDto;
 import todolist.gateway.service.GatewayService;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @RestController
 @RequestMapping("/api/v1/service")
@@ -29,6 +37,25 @@ public class Gateway {
     {
         log.info("process Call");
         return gateway.processCall(data, callUrl, callMethod);
+    }
+    
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @RequestMapping(value="/noti", method=RequestMethod.POST)
+    public ResponseEntity<String> notiBoard(@RequestBody BoardDto boardDto) {
+        Map<String, String> scope = new HashMap<String, String>(){{
+            put("A", "all");
+            put("F", "friends:");
+            put("C", "community:");
+        }};
+        String topic = scope.get(boardDto.getScope_of_disclosure());
+        if( !boardDto.getScope_of_disclosure().equals("A") )
+        {
+            topic += boardDto.getUser_id();
+        }
+        messagingTemplate.convertAndSend("board/"+topic, boardDto);
+        return ResponseEntity.ok("["+LocalDateTime.now()+"]Notification sent to WebSocket clients");
     }
     
     // @RequestMapping(method=RequestMethod.GET)
